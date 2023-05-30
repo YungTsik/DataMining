fa3eca4e374c8dad971170b34df1d7ba80ddb45e
Tsik <64966559+YungTsik@users.noreply.github.com>
2023-05-30 22:53:02 +0300

    Added plots for the clusterings, and added a function for clustering to simplify the script

--- ex2.py
+++ ex2.py
@@ -3,52 +3,51 @@ import pandas as pd
 from sklearn.cluster import KMeans as kmeans
 import seaborn as sns
 
+#Function for kmeans clustering
+def clustering(cluster_data, clusters, result):
+    train = data[cluster_data].to_numpy()
+    k = kmeans(n_clusters = clusters)
+    k.fit(train)
+    predict = lambda X: k.predict(X)
+    clusters = predict(train)
+    result['Cluster'] = clusters
+    print(result)
 
 corona = pd.read_csv("Data/alteredData.csv")
 
 #Clustering 
 data = corona
+#!! maybe scale the data? !!
 data = data.groupby('Entity').max()
 data['Deaths/Cases'] = data['Deaths']/data['Cases']
 data['Cases/Population'] = data['Cases']/data['Population']
 
-result1 = data
-result2 = data
-result3 = data
+result1 = result2 = result3 =  data
 
 #Deaths/Cases based Clustering
-train1 = data[['Deaths/Cases']].to_numpy()
-k1 = kmeans(n_clusters=5)
-k1.fit(train1)
-predict = lambda X: k1.predict(X)
-clusters = predict(train1)
-result1['Cluster'] = clusters
-print("Deaths/Cases Clustering")
-print(result1)
+print('Deaths/Cases Clustering')
+clustering(['Deaths/Cases'], 5, result1)
 
 #Cases/Population based Clustering
-train2 = data[['Cases/Population']].to_numpy()
-k2 = kmeans(n_clusters=5)
-k2.fit(train2)
-predict = lambda X: k2.predict(X)
-clusters = predict(train2)
-result2['Cluster'] = clusters
-print("Cases/Population Clustering")
-print(result2)
+print('Cases/Population Clustering')
+clustering(['Cases/Population'], 5, result2)
 
 #Deaths/Cases and Cases/Population based Clustering
-train3 = data[['Deaths/Cases','Cases/Population']].to_numpy()
-k3 = kmeans(n_clusters=5)
-k3.fit(train3)
-predict = lambda X: k3.predict(X)
-clusters = predict(train3)
-result3['Cluster'] = clusters
-print("Deaths/Cases and Cases/Population Clustering")
-print(result3)
+print('Deaths/Cases', 'Cases/Population Clustering')
+clustering(['Deaths/Cases', 'Cases/Population'], 5, result3)
 
-
+#Heatmap for Pairwise Correlation
 corr = result3.corr().round(2)
 plt.figure("Pairwise Correlation",figsize=(10,10))
 sns.heatmap(corr, annot= True)
+plt.tight_layout()
+plt.show()
+
+#Plots for each clustering
+fig, axs = plt.subplots(ncols=3, figsize=(12,7))
+fig.canvas.manager.set_window_title("Clustering Plots")
+sns.scatterplot(x='Deaths', y='Cases', hue='Cluster', data=result1, ax=axs[0])
+sns.scatterplot(x='Cases', y='Population', hue='Cluster', data=result2, ax=axs[1])
+sns.scatterplot(x='Deaths/Cases', y='Cases/Population', hue='Cluster', data=result3, ax=axs[2])
 plt.tight_layout()
 plt.show()
